19a42fea00e47257bcac73fa89693ebdaa0d0f8e
ronisegoly <roni.segoly@gmail.com>
2016-05-06 07:44:22 +0300

    Update set_dh_actuator.py

--- set_dh_actuator.py
+++ set_dh_actuator.py
@@ -2,10 +2,6 @@ import sys
 import dh
 
 import pdb
-#$value=shell_exec ("python push_dh_sensor.py ".$_GET['project']." ".$_GET['uuid']. " ".$_GET['sensor']." ".$_GET['d_value']);
-
-#def set_actuator_state(actuator,project,uuid,apikey,value):
-
 project = sys.argv[1]
 uuid= sys.argv[2]
 actuator = sys.argv[3]
